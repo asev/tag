dcfa1ec76813a03da4b1bc2aae6d7934b5085dfe
Tomas Pocevicius <tealogija@gmail.com>
2012-12-01 00:32:22 +0200

    Pridėti pirminiai item ir order kontroleriai

--- application/controllers/order.php
+++ application/controllers/order.php
@@ -1,53 +1,41 @@
 <?php
-Class Order extends CI_Controller {
+
+Class Order extends CI_Controller
+{
 
     private $view = "";
+    private $order = null;
+    private $me = null;
 
-public function Order()
-{
-parent::__construct();
-$this->load->library('form_validation');
-$this->load->model('request_model', 'reqM');
-$this->load->model('order_model', 'orderM');
-}
+    public function order()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->model('order_model', 'orderM');
+        $this->load->model('request_model', 'reqM');
+    }
 
-public function index()
-{
-    redirect('');
-}
-
-public function add($reqId=null)
-{
-    if (!$this->tank_auth->is_logged_in()) {
+    public function index()
+    {
         redirect('');
-    } else {
-        if ($this->reqM->getManager($reqId) == $this->tank_auth->get_user_id()){
-            $data = array();
-            // Validuojam visus input.
-            $this->form_validation->set_rules('full-name', 'Full name', 'trim|required|max_length[100]|xss_clean');
-            $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
-            $this->form_validation->set_rules('phone', 'Phone number', 'trim|min_length[6]|max_length[50]');
-            $this->form_validation->set_rules('request-text', 'Request', 'trim|required|min_length[10]|max_length[5000]');
+    }
 
-            // Jeigu pavyko validacija
-            if($this->form_validation->run())
-            {
-                $this->orderM->addOrder();
-                $this->view = $this->view . $this->load->view('order/success', true);
-            }
-            $data['reqId'] = $reqId;
+    public function add($reqId = null)
+    {
+        if (!$this->tank_auth->is_logged_in()) {
+            redirect('');
+        } else {
+            $this->me = $this->tank_auth->get_user_id();
+            $this->orderM->addOrder($reqId, $this->me);
+            $this->order = $this->orderM->getOrder($reqId, $this->me);
+            $data = get_object_vars($this->order);
             $this->view = $this->view . $this->load->view('order/form', $data, true);
         }
-        else {
-            redirect('req/show'. $reqId);
-        }
+        $this->displayer->DisplayView($this->view);
     }
-    $this->load->view('page', array('view' => $this->view));
-}
 
-public function generatePdf($oderId)
-{
+    public function finish($orderId = null)
+    {
 
-}
-
+    }
 }
