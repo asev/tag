56f5158a57ac9044be857fcc312ca3e0a83c0587
Tomas Pocevicius <tealogija@gmail.com>
2012-12-01 00:33:50 +0200

    Pridėti pirminiai item ir order view'ai

--- application/views/order/form.php
+++ application/views/order/form.php
@@ -1,13 +1,28 @@
-<?php   //@toGin paruošti formą pagal duombazę. Įvedamas kodas, pavadinimas, kaina ir kiekis
-        // Sito daryti neskubek. Tikriausiai bus keitimų
+<?php
+
+$comment = array(
+    'name'  => 'order-comment',
+    'id'    => 'order-comment',
+    'value' => set_value('order-comment'),
+    'maxlength'	=> 2000,
+
+    'size'	=> 200,
+);
+
 ?>
 <div id="order-form">
-    <?php echo validation_errors(); ?>
-    <?php echo form_open(''); ?>
-    <input type="text" name="full-name" value="" size="50" />
-    <input type="text" name="email" value="" size="50" />
-    <input type="text" name="phone" value="" size="50" />
-    <input type="text" name="request-text" value="" size="50" />
-    <input type="submit" />
-    <?php echo form_close(); ?>
+    <?php if(isset($get_items)): ?>
+    <?php echo '<table border="1">'; ?>
+    <?php echo '<tr><td>Prekės Id</td><td>Pavadinimas</td><td>Kaina</td><td>Kiekis</td><td></td></td></tr>'; ?>
+    <?php foreach($get_items as $row): ?>
+        <?php echo '<tr><td>'.$row['itemId'].'</td>'; ?>
+        <?php echo '<td>'.$row['itemName'].'</td>'; ?>
+        <?php echo '<td>'.$row['itemPrice'].'</td>'; ?>
+        <?php echo '<td>'.$row['itemQuantity'].'</td>'; ?>
+        <?php echo '<td>'.anchor('item/delete/' . $orderId . '/' . $row['itemId'], 'Ištrinti').'</td>'; ?>
+        <?php echo '</tr>'; ?>
+        <?php endforeach; ?>
+    <?php echo '</table>'; ?>
+    <?php endif; ?>
+    <?php echo anchor('item/add/' . $orderId, 'Pridėti prekę'); ?>
 </div>
